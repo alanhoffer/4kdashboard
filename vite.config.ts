9206172914ae5a5b61ecf695ebb9e6e753ffd129
Alan Hoffer <alanhoffer97@hotmail.com>
2025-08-04 12:59:33 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    allowedHosts: ['apitoolbackend.ddns.net'],
+  },
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
