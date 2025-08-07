5b7ea74c6ef6e4c726f45f24113f3ffc321c859d
alan hoffer <alanhoffer97@hotmail.com>
2025-08-07 12:46:11 -0300

    asd

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -91,7 +91,7 @@ const FileUpload = () => {
     formData.append('clientId', dealer.clientId || '');
 
     try {
-      const response = await axios.post('http://4k.cabañahoffer.com.ar/api/upload', formData, {
+      const response = await axios.post('https://4k.cabañahoffer.com.ar/api/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
